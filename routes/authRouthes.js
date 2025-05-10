8e055337dd7e5a137c860c3a4a196e5001747c74
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-10 10:29:38 +0200

    använder bcrypt för att hasha lösenord

--- routes/authRouthes.js
+++ routes/authRouthes.js
@@ -1,39 +1,69 @@
 const express = require("express");
 const router = express.Router();
+require("dotenv").config();
+const sqlite3 = require("sqlite3").verbose();
+const bcrypt = require("bcrypt");
+
+// Connect to database
+const db = new sqlite3.Database(process.env.DATABASE);
 
 // Add a new user
 router.post("/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    // Validation - vidare utveckla senare
-    if (!username || !password) {
-        return res.status(400).json({ error: "You must fill in your username/password."});
-    }
+    try {
+        const { username, password } = req.body;
+        // Validation - vidare utveckla senare
+        if (!username || !password) {
+            return res.status(400).json({ error: "You must fill in your username/password." });
+        }
+        // Check if user is already existing - utveckla
 
-    // Correct inut
-    res.status(201).json({ message: "User created"});
-  } catch (error) {
-    res.status(500).json({ error: "Server error"});
-  }
+        // Hash password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        // Correct input
+        const sql = `INSERT INTO users(username, password) VALUES(?,?)`;
+        db.run(sql, [username, hashedPassword], (err) => {
+            if (err) {
+                res.status(400).json({message: "Error creating user..."});
+            } else {
+                res.status(201).json({ message: "User created" });
+
+            }
+
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
 });
 
 // Login user
-router.post("/login", async(req, res) => {
+router.post("/login", async (req, res) => {
     try {
-     const { username, password } = req.body;
-    // Validation - vidare utveckla senare
-    if (!username || !password) {
-        return res.status(400).json({ error: "You must fill in your username/password."});
-    }
-   // Hårdkodat
-   if(username === "Nathalie" && password === "hej") {
-    res.status(200).json({message: "Login successful"});
-   } else {
-    res.status(401).json({message: "Incorrect username/password"});
-   }
+        const { username, password } = req.body;
+        // Validation - vidare utveckla senare
+        if (!username || !password) {
+            return res.status(400).json({ error: "You must fill in your username/password." });
+        }
+       // Check if user exists
+       const sql = `SELECT * FROM users WHERE username =?`;
+       db.get(sql, [username], async (err, row) => {
+        if(err) {
+            res.status(400).json({message: "Error authenticating..."});
+        } else if (!row) {
+            res.status(401).json({message: "Incorrect username/password!"});
+        } else  {
+            // Check password
+            const passwordMatch = await bcrypt.compare(password, row.password);
 
-    } catch(error) {
-        res.status(500).json({error: "Server error"});
+            if (!passwordMatch) {
+                res.status(401).json({message: "Incorrect username/password!"});
+            } else {
+                res.status(200).json({message: "Correct login!"});
+            }
+        }
+       });
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
     }
 });
 
