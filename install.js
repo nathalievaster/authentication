4a69670cf5f2d4f3ccb2a99a07ca3fc9be207a6d
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-10 10:05:36 +0200

    skapar tabell med sqlite3, något att bygga vidare

---
+++ install.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database(process.env.DATABASE);
+
+// Create table users
+db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS users");
+
+    db.run(`CREATE TABLE users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        created DATETIME DEFAULT CURRENT_TIMESTAMP
+        )`);
+
+        console.log("Table created...");
+});
+
