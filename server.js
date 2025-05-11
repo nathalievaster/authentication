9df2161c70616d98c088283abe452c876048d239
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-11 22:49:19 +0200

    glömde app.use...

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const authRouthes = require("./routes/authRouthes");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use("/api", authRouthes);
