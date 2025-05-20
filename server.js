dd5dfb53962cb9577e18acd939644b963cddc8ff
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-20 21:42:39 +0200

    hämtar listan med användare på skyddad route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Importer of packages
 const cors = require("cors");
 const express = require("express");
+const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -14,24 +15,36 @@ app.use(cors());
 // Routes
 app.use("/api", authRouthes);
 
-// Protected routes 
-app.get("/api/secret", authenticateToken, (req, res) => {
-    res.json ({ message: "Skyddad route!"});
-})
-
 // Validate token 
-function authenticateToken(req, res, next) { 
-    const authHeader = req.headers ['authorization'];
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) res.status(401).json ({ message: "You have no access to this route, missing token!"});
+    if (!token) {
+        return res.status(401).json({ message: "You have no access to this route, missing token!" });
+    }
+
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-        if(err) return res.status(403).json({ message: "Unvalid JWT"});
+        if (err) {
+            return res.status(403).json({ message: "Unvalid JWT" });
+        }
 
         req.user = user;
         next();
     });
 }
+
+app.get("/api/secret", authenticateToken, (req, res) => {
+    const sql = `SELECT id, username, created FROM users`;
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ message: "Database error" });
+        }
+
+        res.json({ users: rows });
+    });
+});
 
 // Starting upp the application
 app.listen(port, () => {
